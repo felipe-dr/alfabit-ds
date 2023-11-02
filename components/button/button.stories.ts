39aeb6f439868920d1e9a72ce3fee356c334cb7c
Felipe DR <felipe.corp7@gmail.com>
2023-11-02 13:41:46 -0300

    feat: class-04 button component

--- components/button/button.stories.ts
+++ components/button/button.stories.ts
@@ -5,7 +5,17 @@ import Button, { ButtonProps } from './button';
 const meta: Meta<ButtonProps> = {
   title: 'Molecules/Button',
   component: Button,
-  argTypes: {},
+  argTypes: {
+    children: {
+      type: 'string',
+    },
+    disabled: {
+      type: 'boolean',
+    },
+    className: {
+      type: 'string',
+    },
+  },
 };
 
 export default meta;
@@ -16,9 +26,62 @@ export const Primary: StoryObj<ButtonProps> = {
   },
 };
 
+export const Disabled: StoryObj<ButtonProps> = {
+  args: {
+    children: 'Botão',
+    disabled: true,
+  },
+};
+
+export const Secondary: StoryObj<ButtonProps> = {
+  args: {
+    children: 'Botão',
+    variant: 'secondary',
+  },
+};
+
+export const SecondaryDisabled: StoryObj<ButtonProps> = {
+  args: {
+    children: 'Botão',
+    variant: 'secondary',
+    disabled: true,
+  },
+};
+
+export const Tertiary: StoryObj<ButtonProps> = {
+  args: {
+    children: 'Botão',
+    variant: 'tertiary',
+  },
+};
+
+export const TertiaryDisabled: StoryObj<ButtonProps> = {
+  args: {
+    children: 'Botão',
+    variant: 'tertiary',
+    disabled: true,
+  },
+};
+
 export const Violet: StoryObj<ButtonProps> = {
   args: {
     className: 'theme-violet',
+    children: 'Botão',
+  },
+};
+
+export const VioletSecondary: StoryObj<ButtonProps> = {
+  args: {
+    className: 'theme-violet',
+    variant: 'secondary',
+    children: 'Botão',
+  },
+};
+
+export const VioletTertiary: StoryObj<ButtonProps> = {
+  args: {
+    className: 'theme-violet',
+    variant: 'tertiary',
     children: 'Botão',
   },
 };
